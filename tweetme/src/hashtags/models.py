146f3e61fd2d0e69d7505d4933ab0c8a9aeb9054
Shraddha2702 <shraddhak102@gmail.com>
2017-10-29 15:47:03 +0530

    commit

---
+++ tweetme/src/hashtags/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.urls import reverse_lazy
+# Create your models here.
+
+from tweets.models import Tweet
+from .signals import parsed_hashtags
+
+class HashTag(models.Model):
+    tag = models.CharField(max_length=120)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self): # __unicode__
+        return self.tag
+
+    def get_absolute_url(self):
+        return reverse_lazy("hashtag", kwargs={"hashtag": self.tag})
+
+    def get_tweets(self):
+        return Tweet.objects.filter(content__icontains="#" + self.tag)
+
+
+
+
+def parsed_hashtags_receiver(sender, hashtag_list, *args, **kwargs):
+    if len(hashtag_list) > 0:
+        for tag_var in hashtag_list:
+            new_tag, create = HashTag.objects.get_or_create(tag=tag_var)
+
+
+parsed_hashtags.connect(parsed_hashtags_receiver)
